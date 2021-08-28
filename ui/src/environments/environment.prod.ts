5bd69edaa1ff5b50cb941add054e5ed53891871b
incredibledevelopers <56466746+incredibledevelopers@users.noreply.github.com>
2021-08-28 23:37:09 +0530

    Added Port no in env

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   title: 'Production Environment Heading',
-  apiUrl: 'http://150.242.14.192/audit/'
+  apiUrl: 'http://150.242.14.192:8085/audit/'
 };
